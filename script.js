56d9e1a88e85c648cb18666bac0d831ab4ff710a
XxWolf29xX <140122267+XxWolf29xX@users.noreply.github.com>
2023-08-24 22:34:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//1er ejercicio
+var nombre="Claudio";
+var apellido="Levato";
+console.log(nombre+" "+apellido);
+
+//2do ejercicio
+var num1=1;
+var num2=2;
+var suma=num1+num2;
+var num3=suma+3;
+console.log(suma);
+console.log(num3);
